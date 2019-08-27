8527cde26ef9ad880d771f6f0c41236d69868ac9
manish-giri <manish.giri.me@gmail.com>
2019-08-26 22:39:49 -0400

    Commit spring boot starter project

---
+++ firstwebapp/src/main/java/com/manishgiri/springbootwebapp/SpringbootFirstwebappApplication.java
@@ -0,0 +1,13 @@
+package com.manishgiri.springbootwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFirstwebappApplication.class, args);
+    }
+
+}
