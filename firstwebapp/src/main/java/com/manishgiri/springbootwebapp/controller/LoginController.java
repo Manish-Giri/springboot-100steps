58bdb8e65f7934c9369ed27f778d04e542f0d04c
MANISH-GIRI <manish.giri.me@gmail.com>
2019-10-15 23:41:56 -0400

    Create a POST handler to implement login

--- firstwebapp/src/main/java/com/manishgiri/springbootwebapp/controller/LoginController.java
+++ firstwebapp/src/main/java/com/manishgiri/springbootwebapp/controller/LoginController.java
@@ -1,15 +1,33 @@
 package com.manishgiri.springbootwebapp.controller;
 
+import com.manishgiri.springbootwebapp.service.LoginService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
+    LoginService loginService = new LoginService();
+
     @RequestMapping("/login")
     //@ResponseBody
-    public String loginResponse() {
+    public String showLoginPage() {
         return "login";
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String showWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model) {
+        if(loginService.validateUser(name, password)) {
+            model.put("name", name);
+            return "welcome";
+        }
+        else
+            return "login";
+        // return "welcome";
+    }
+
 }
